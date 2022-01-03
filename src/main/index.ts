2ce64761a681cd86bcfd41782b4210e48b9b6c3a
jet2jet <34238471+jet2jet@users.noreply.github.com>
2022-01-03 12:30:55 +0900

    Add NtExecutableResource class to treat resource entry data

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,16 +2,20 @@ import NtExecutable, {
 	NtExecutableFromOptions,
 	NtExecutableSection,
 } from './NtExecutable';
+import NtExecutableResource from './NtExecutableResource';
 import { calculateCheckSumForPE } from './util/functions';
 import version from './version';
 
 import * as Format from './format';
+import * as Type from './type';
 
 export {
 	NtExecutable,
 	NtExecutableFromOptions,
+	NtExecutableResource,
 	NtExecutableSection,
 	calculateCheckSumForPE,
 	version,
 	Format,
+	Type,
 };
---
+++ src/main/type/index.ts
@@ -0,0 +1,12 @@
+import ResourceEntry, {
+	ResourceEntryBaseType,
+	ResourceEntryT,
+	ResourceEntryTT,
+} from './ResourceEntry';
+
+export {
+	ResourceEntry,
+	ResourceEntryBaseType,
+	ResourceEntryT,
+	ResourceEntryTT,
+};
